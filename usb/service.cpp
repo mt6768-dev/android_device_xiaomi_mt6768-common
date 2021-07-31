2fd969e2ed5fb866b947b2ca22b17a1680fb8cd8
Ash Blake <ash.blake@tutanota.com>
2021-07-31 21:28:26 +0200

    mt6768-common: usb: unify Usb and UsbGadget services

--- usb/service.cpp
+++ usb/service.cpp
@@ -18,6 +18,7 @@
 
 #include <hidl/HidlTransportSupport.h>
 #include "Usb.h"
+#include "UsbGadget.h"
 
 using android::sp;
 
@@ -26,6 +27,8 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
+using android::hardware::usb::gadget::V1_1::IUsbGadget;
+using android::hardware::usb::gadget::V1_1::implementation::UsbGadget;
 using android::hardware::usb::V1_1::IUsb;
 using android::hardware::usb::V1_1::implementation::Usb;
 
@@ -34,12 +37,20 @@ using android::status_t;
 
 int main() {
   android::sp<IUsb> service = new Usb();
+  android::sp<IUsbGadget> service2 = new UsbGadget();
 
-  configureRpcThreadpool(1, true /*callerWillJoin*/);
+  configureRpcThreadpool(2, true /*callerWillJoin*/);
   status_t status = service->registerAsService();
 
   if (status != OK) {
     ALOGE("Cannot register USB HAL service");
+    return 1;
+  }
+
+  status = service2->registerAsService();
+
+  if (status != OK) {
+    ALOGE("Cannot register USB Gadget HAL service");
     return 1;
   }
 
