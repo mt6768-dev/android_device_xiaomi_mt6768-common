94913ab2ff0a20e772fb45f63c3bfe0c3b51e8e1
Demon000 <demonsingur@gmail.com>
2021-10-10 16:20:42 +0100

    mt6768-common: init: cleanup includes

--- init/init.cpp
+++ init/init.cpp
@@ -14,20 +14,15 @@
  * limitations under the License.
  */
 
-#include <cstdlib>
-#include <fstream>
-#include <string.h>
+#include <cstring>
 #include <sys/sysinfo.h>
-#include <unistd.h>
 
-#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 
-using android::base::GetProperty;
 using android::init::property_set;
 
 void load_dalvik_properties()
