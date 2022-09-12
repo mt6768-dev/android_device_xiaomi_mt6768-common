9c1ade0e5dd3bb92ea1950ed47db6eab7578525e
bengris32 <bengris32@protonmail.ch>
2022-09-12 20:25:29 +0100

    mt6768-common: Build required Audio libs
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -10,6 +10,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl
+
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
+    libtinycompress \
+    libtinyxml \
+    tinymix
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
