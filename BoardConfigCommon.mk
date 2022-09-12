cb6c9b240e84c52805566344665e6715f045f40c
bengris32 <bengris32@protonmail.ch>
2022-09-12 18:37:15 +0100

    mt6768-common: Define architecture flags
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,20 @@
 
 COMMON_PATH := device/xiaomi/mt6768-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
 # Inherit the proprietary files
 include vendor/xiaomi/mt6768-common/BoardConfigVendor.mk
