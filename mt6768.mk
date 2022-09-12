e4fb7ed26d7f3fdbdfd1d0a814ad5ecbfede9145
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-12 19:05:04 +0100

    mt6768-common: Enforce RRO for all overlays

--- mt6768.mk
+++ mt6768.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
