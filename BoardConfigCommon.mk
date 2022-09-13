e878afaaa3e39107daa5939e35fe6ac8920c8bbd
bengris32 <bengris32@protonmail.ch>
2022-09-13 19:16:57 +0100

    [DNM] mt6768-common: Set SELinux to permissive
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,6 +50,7 @@ BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
