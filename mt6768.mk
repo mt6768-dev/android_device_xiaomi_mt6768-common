6a53b1ef55c861d5bb6b2d2f535844bd3d605551
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-13 18:54:08 +0100

    mt6768-common: Build default health HIDL

--- mt6768.mk
+++ mt6768.mk
@@ -70,6 +70,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
