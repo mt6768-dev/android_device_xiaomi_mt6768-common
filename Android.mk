7e97bacaaa1ffc945b86dba96e2accf94f019555
Kshitij Gupta <kshitijgm@gmail.com>
2022-09-13 19:05:26 +0100

    mt6768-common: Create symlinks for vullkan - vulkan blobs are symlinks to libGLES_mali.so

--- Android.mk
+++ Android.mk
@@ -8,4 +8,15 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter merlinx lava,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+VULKAN_SYMLINKS := $(TARGET_OUT_VENDOR)
+$(VULKAN_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating vulkan symlinks: $@"
+	@mkdir -p $@/lib/hw/
+	@mkdir -p $@/lib64/hw/
+	@ln -sf $@/lib/egl/libGLES_mali.so $@/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	@ln -sf $@/lib64/egl/libGLES_mali.so $@/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINKS)
+
 endif
