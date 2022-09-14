9e780ab30b16c21e0cb19828318b07f26e931ca4
Volodymyr Zhdanov <wight554@gmail.com>
2022-09-14 19:47:17 +0100

    mt6768-common: Disable SF configstore * ship disable_configstore because we have API level 29

--- mt6768.mk
+++ mt6768.mk
@@ -51,6 +51,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor
 
+# Disable SF configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
