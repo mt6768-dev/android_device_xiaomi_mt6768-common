de3fc079faca98742d12421867c077a4f1b7a9bb
bengris32 <bengris32@protonmail.ch>
2022-09-14 19:50:54 +0100

    mt6768-common: Build default Memtrack HIDL service
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -60,6 +60,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service
 
 PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+PRODUCT_PACKAGES += \
     android.hardware.graphics.common@1.2.vendor \
     android.hardware.graphics.mapper@4.0.vendor \
     android.hardware.graphics.allocator@4.0.vendor
