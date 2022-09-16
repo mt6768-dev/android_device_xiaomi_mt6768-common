e340b3b390f8110c4143ac4845132411f701e23a
bengris32 <bengris32@protonmail.ch>
2022-09-16 21:53:03 +0100

    mt6768-common: Build android.hidl.allocator@1.0
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -118,7 +118,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.allocator@1.0 \
     android.hidl.base@1.0.vendor \
+    android.hidl.allocator@1.0.vendor \
     libhidltransport.vendor \
     libhwbinder.vendor
 
