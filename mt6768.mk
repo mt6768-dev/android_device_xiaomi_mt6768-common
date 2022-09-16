f6eba460f7ff5ef52ede000652bc7def61f9d3e5
bengris32 <bengris32@protonmail.ch>
2022-09-16 21:59:07 +0100

    mt6768-common: Switch to basic USB service * AOSP default one is of course broken on MTK. Sigh.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -163,7 +163,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
