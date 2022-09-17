8c8483f9a246a472e47db4b4d22619668fa11bf8
XiNGRZ <chenxingyu92@gmail.com>
2022-09-17 11:11:59 +0100

    mt6768-common: Add dtbo image to the output zip if present

--- releasetools.py
+++ releasetools.py
@@ -34,4 +34,5 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   return
