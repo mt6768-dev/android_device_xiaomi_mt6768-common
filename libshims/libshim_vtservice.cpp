75e67a407a1bb551918367b130b0003e9196763f
R0rt1z2 <rogerortizleal@gmail.com>
2022-09-18 12:04:18 +0100

    mt6768-common: Shim another missing libsink symbol [1] LineageOS/android_frameworks_native@992496b

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -1,3 +1,4 @@
+#include <gui/SurfaceComposerClient.h>
 #include <media/AudioTrack.h>
 
 using namespace android;
@@ -27,4 +28,13 @@ void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_o
                             user, notificationFrames, sessionId, transferType, offloadInfo, attributionSource,
                             pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId);
         }
+
+void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
+    void* thisptr, const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp, const sp<IBinder>& surfaceControlHandle);
+
+void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
+    void* thisptr, const sp<IGraphicBufferProducer> &bufferProducer, bool controlledByApp) {
+        _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(thisptr, bufferProducer, controlledByApp, nullptr);
+}
+
 }
