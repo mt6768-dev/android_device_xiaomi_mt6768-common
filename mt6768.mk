491150b400b9f917a1b97b04eb31a70885f4934e
bengris32 <bengris32@protonmail.ch>
2022-09-18 09:13:30 +0100

    mt6768-common: Build Health 2.0 interface
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -122,6 +122,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
