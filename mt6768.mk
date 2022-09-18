e00a8e57814ed8a3ac03dfdab982bfbc59c26c44
Kshitij Gupta <kshitijgm@gmail.com>
2022-09-18 11:59:04 +0100

    mt6768-common: Add IMS blobs

--- mt6768.mk
+++ mt6768.mk
@@ -136,6 +136,16 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
