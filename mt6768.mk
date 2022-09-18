aa28eb05aaa1dfefcb9101197c26d52e173232d4
Danny Baumann <dannybaumann@web.de>
2022-09-18 23:01:43 +0100

    mt6768-common: Enable call recording

--- mt6768.mk
+++ mt6768.mk
@@ -189,7 +189,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     WifiOverlay \
