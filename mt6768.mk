6273905ad9f910650726dbdd90e46c30b4b75e28
TheMalachite <eliasgheeraert@gmail.com>
2022-09-19 09:23:20 +0100

    mt6768-common: Add Mediatek IMS permissions

--- mt6768.mk
+++ mt6768.mk
@@ -146,6 +146,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
