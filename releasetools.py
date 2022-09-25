13178c70917fcddd59fdf460da93c822c26db8f3
bengris32 <bengris32@protonmail.ch>
2022-09-25 21:18:40 +0100

    mt6768-common: Also build vbmeta images for system and vendor
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- releasetools.py
+++ releasetools.py
@@ -34,5 +34,7 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+  AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
+  AddImage(info, "vbmeta_vendor.img", "/dev/block/by-name/vbmeta_vendor")
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   return
