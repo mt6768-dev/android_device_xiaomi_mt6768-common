bbea7322f059f9d631cef9420ca6fabf5445e4f6
Bowgo Tsai <bowgotsai@google.com>
2022-10-01 15:54:47 +0100

    mt6768-common: Adding developer GSI keys
    Bug: 144399552
    Test: tree-hugger

--- mt6768.mk
+++ mt6768.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service.mediatek \
