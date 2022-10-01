7f00eadbff1f099ab8fd502b84fcb7f6bf4ab897
Josh Wu <joshwu@google.com>
2022-10-01 14:03:36 +0100

    mt6768-common: Switch BtAudio to AIDL Test: m Bug: 203490261

--- mt6768.mk
+++ mt6768.mk
@@ -18,7 +18,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service.mediatek \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.bluetooth.audio@2.1-impl
+    android.hardware.bluetooth.audio-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
