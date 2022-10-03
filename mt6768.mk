c9fd5eb3291dd9f8d2dfbde20fa2cfde2abb1ca5
bengris32 <bengris32@protonmail.ch>
2022-10-03 10:19:10 +0100

    mt6768-common: Uprev Clearkey DRM HAL to 1.4
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -79,7 +79,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
     libmockdrmcryptoplugin
