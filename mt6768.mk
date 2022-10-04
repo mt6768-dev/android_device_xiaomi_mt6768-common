71a372d2f9aa0a51d2bd5c14f58b4bb2f24fe664
bengris32 <bengris32@protonmail.ch>
2022-10-04 20:04:01 +0100

    mt6768-common: Drop MediaTek PowerHAL * We still have to keep some vendor interfaces and libs   since some HALs have dependencies on them.
    * Also switch to stub AIDL power service until we can get
      libperfmgr properly implemented.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -230,18 +230,13 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-mediatek
+    android.hardware.power-service.example
 
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor \
     android.hardware.power@1.3.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
-    $(LOCAL_PATH)/configs/power/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
-    $(LOCAL_PATH)/configs/power/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
