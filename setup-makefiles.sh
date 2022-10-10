9573e0e12d2ce6225a4995de58ac82867417a39f
Matsvei Niaverau <matvej2002@gmail.com>
2022-10-10 22:52:43 +0300

    fixup! mt6768-common: Update build guard to check for lancelot instead of lava
    Change-Id: I5c7950f7b48c770b5a294fb700837573a70cd146

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "merlinx lava"
+write_headers "merlinx lancelot"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
