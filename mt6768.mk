5fdca4dd07b9f824e3970543ada7adbe654c628b
Daniel Rosenberg <drosen@google.com>
2022-11-28 16:09:23 +0000

    mt6768-common: Enable Project ID Quota support on userdata.  * These features enable wide inodes on userdata, which    is required for project ID quotas. Wide inodes can    only be set at MKFS time.
     * This also enables casefolding, which is required for the
       removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67

--- mt6768.mk
+++ mt6768.mk
@@ -13,6 +13,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service.mediatek \
