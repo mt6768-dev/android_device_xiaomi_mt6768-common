5e4adbec0cdbac754b9e0cb4da1a50273b5d8a45
bengris32 <bengris32@protonmail.ch>
2023-02-01 22:00:19 +0000

    mt6768-common: InCallService: Restore gain levels on boot and call initiation
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I68194d5301a87380196f4003881541dffadf3ae4

--- app/InCallService/src/org/lineageos/mediatek/incallservice/GainUtils.java
+++ app/InCallService/src/org/lineageos/mediatek/incallservice/GainUtils.java
@@ -1,5 +1,6 @@
 package org.lineageos.mediatek.incallservice;
 
+import android.media.AudioDeviceInfo;
 import android.os.SystemProperties;
 import android.media.AudioSystem;
 import android.util.Log;
@@ -17,5 +18,14 @@ public class GainUtils {
                                           streamType);
         Log.d(LOG_TAG, "Setting audio parameters to: " + parameters);
         AudioSystem.setParameters(parameters);
+    }
+
+    /**
+     * Sets the gain level for built-in earpiece and bluetooth SCO devices.
+     * @param gainIndex The gain level to set.
+     */
+    public static void setGainLevel(int gainIndex) {
+        GainUtils.setGainLevel(AudioDeviceInfo.TYPE_BUILTIN_EARPIECE, gainIndex, AudioSystem.STREAM_VOICE_CALL);
+        GainUtils.setGainLevel(AudioDeviceInfo.TYPE_BLUETOOTH_SCO, gainIndex, AudioSystem.STREAM_VOICE_CALL);
     }
 }
