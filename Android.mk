cf464979386803333bcd109241f4d276e1925d67
Kshitij Gupta <kshitijgm@gmail.com>
2023-06-02 19:39:18 +0200

    mt6768-common: Create required vendor symlinks for gatekeeper
    Change-Id: I0d87c2b4332c0bcff0a7872fffd13e8ec1225c6e

--- Android.mk
+++ Android.mk
@@ -9,14 +9,22 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter merlinx lancelot,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-VULKAN_SYMLINKS := $(TARGET_OUT_VENDOR)
-$(VULKAN_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating vulkan symlinks: $@"
-	@mkdir -p $@/lib/hw/
-	@mkdir -p $@/lib64/hw/
-	@ln -sf $@/lib/egl/libGLES_mali.so $@/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
-	@ln -sf $@/lib64/egl/libGLES_mali.so $@/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+VENDOR_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib \
+    $(TARGET_OUT_VENDOR)/lib64 \
+    $(TARGET_OUT_VENDOR)/lib/hw \
+    $(TARGET_OUT_VENDOR)/lib64/hw
 
-ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINKS)
+$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Making vendor symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
 
 endif
