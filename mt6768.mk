f31a82bd484100a008935e83c10a7da8587b9676
Matsvei Niaverau <matvej2002@gmail.com>
2023-06-22 14:44:10 +0200

    Revert "mt6768-common: Build Lineage Health HAL"
    This reverts commit 356980a835e8ad7686e39391e37ae1b495ca7ea2.
    
    Change-Id: I828a30fad11ebfe7adfabf56a1dc43fc88986cfe

--- mt6768.mk
+++ mt6768.mk
@@ -206,10 +206,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.mt6768
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
