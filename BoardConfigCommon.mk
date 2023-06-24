0b2e280a7fabd533937a943844b1743c2f3edb52
Sarthak Roy <sarthakroy2002@gmail.com>
2023-06-24 22:58:01 +0530

    mt6768-common: sepolicy: Fix property violations
    Change-Id: I736d314c2d9bb1444e542739a7b6b70c62395580

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,6 +133,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # SPL
 VENDOR_SECURITY_PATCH := 2022-03-01
