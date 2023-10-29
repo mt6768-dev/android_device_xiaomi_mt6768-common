b7bb8f50cc3c655b58458cccac5a1accbd96f93a
Gabriel Biren <gbiren@google.com>
2023-10-29 16:42:03 +0000

    mt6768-common: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- mt6768.mk
+++ mt6768.mk
@@ -392,7 +392,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     wpa_supplicant \
     hostapd \
-    android.hardware.wifi@1.0-service-lazy
+    android.hardware.wifi-service
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
