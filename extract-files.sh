3a016dd2ebe7eb36f5692b7a34c3c86d398081b5
SamarV-121 <samarvispute121@pm.me>
2024-02-10 16:41:41 +0100

    mt6768-common: Use libutils-v32.so for thermal HAL
    https://pastebin.com/raw/cQKDwuHP
    
    Change-Id: I5cdd2e4493d9d66c7de2f59a83d6db53e7f51884

--- extract-files.sh
+++ extract-files.sh
@@ -89,7 +89,8 @@ function blob_fixup {
         vendor/lib*/hw/dfps.mt6768.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
-        vendor/lib*/hw/vendor.mediatek.hardware.pq@2.6-impl.so)
+        vendor/lib*/hw/vendor.mediatek.hardware.pq@2.6-impl.so |\
+        vendor/lib*/hw/android.hardware.thermal@2.0-impl.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
 	vendor/lib64/libmi_watermark.so)
